0205c632740f5f04a9ecd8103ddf7ed95fafae57
programmer-alt <modstils88@mail.ru>
2023-11-30 16:07:34 +0400

    routing used useLocation

--- src/Routing/UserId.js
+++ src/Routing/UserId.js
@@ -1,13 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-function UserId (){
-   let {userName} = useParams()
-    return (
-<div>
-    <a href="/Routing/Users">Назад</a>
-    <h1> Users : {userName} </h1>
-</div>
-    )
+function UserId() {
+  let { userName } = useParams();
+
+  return (
+    <div>
+      <a href="/Routing/Users">Назад</a>
+      <h1> Users: {userName} </h1>
+    </div>
+  );
 }
 
 export default UserId;
